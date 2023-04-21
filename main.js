e69fd3e93bcd4ad746dd997aa19b3796abbdcd2a
GaelTikeng <Gaelinho@yahoo.fr>
2023-04-21 11:38:24 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+let amount = document.querySelector('.amount')
+let percentage = document.querySelector('.percentage')
+
+function calculatePercentage() {  
+  let u = parseInt(percentage.value)
+  let amt = parseInt(amount.value)
+  let x = ((u/100) * amt)
+  if (amount.value === '' && percentage.value === '') {
+    document.querySelector('.display').innerHTML = 'Please enter amount'
+  } else {
+    document.querySelector('.display').innerHTML = `The TIP is ${x} FCFA`
+  }
+  
+}
