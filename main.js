dde904957f8792674ccfc3f6bbe575269189d166
GaelTikeng <Gaelinho@yahoo.fr>
2023-04-21 11:53:58 +0100

    new if condition

--- main.js
+++ main.js
@@ -5,8 +5,8 @@ function calculatePercentage() {
   let u = parseInt(percentage.value)
   let amt = parseInt(amount.value)
   let x = ((u/100) * amt)
-  if (amount.value === '' && percentage.value === '') {
-    document.querySelector('.display').innerHTML = 'Please enter amount'
+  if (amount.value === '' || percentage.value === '') {
+    document.querySelector('.display').innerHTML = 'Please fild both fields'
   } else {
     document.querySelector('.display').innerHTML = `The TIP is ${x} FCFA`
   }
